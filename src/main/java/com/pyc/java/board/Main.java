e3e83fce9e12e0ef6dc6143adb9de6a446a3e21a
zerochani <youngchan0510@gmail.com>
2025-03-09 00:45:06 +0900

    게시물 작성시 번호 증가

--- src/main/java/com/pyc/java/board/Main.java
+++ src/main/java/com/pyc/java/board/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("== 자바 텍스트 게시판 시작 ==");
         Scanner sc = new Scanner(System.in);
-
+        int lastId = 0; //게시물 번호
         while(true){
             System.out.print("명령) ");
             String cmd = sc.nextLine();
@@ -19,7 +19,7 @@ public class Main {
                 System.out.print("내용 : ");
                 String content = sc.nextLine();
 
-                int id = 1;
+                int id = ++lastId;
 
                 System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
             }else if(cmd.equals("exit")){
