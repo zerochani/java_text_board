aee00c3d76d195fd958fad850aefbcf5e98f1238
zerochani <youngchan0510@gmail.com>
2025-03-09 00:38:19 +0900

    게시물 작성 기능 구현

--- src/main/java/com/pyc/java/board/Main.java
+++ src/main/java/com/pyc/java/board/Main.java
@@ -6,11 +6,33 @@ public class Main {
         System.out.println("== 자바 텍스트 게시판 시작 ==");
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("명령) ");
-        String cmd = sc.nextLine();
+        while(true){
+            System.out.print("명령) ");
+            String cmd = sc.nextLine();
 
-        System.out.printf("입력받은 명령어 : %s\n", cmd);
+            if(cmd.equals("/user/article/write")){
+                System.out.println("== 게시물 작성 ==");
+
+                System.out.print("제목 : ");
+                String subject = sc.nextLine();
+
+                System.out.print("내용 : ");
+                String content = sc.nextLine();
+
+                int id = 1;
+
+                System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+            }else if(cmd.equals("exit")){
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }else{
+                System.out.println("잘못된 명령어입니다.");
+            }
+            System.out.printf("입력받은 명령어 : %s\n", cmd);
+
+        }
         System.out.println("== 자바 텍스트 게시판 끝 ==");
+        sc.close();
 
     }
 }
