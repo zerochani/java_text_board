7e450ccbf88307fcd2950e369cc84bc0a79fa515
zerochani <youngchan0510@gmail.com>
2025-03-09 00:19:37 +0900

    명령어 입력

--- src/main/java/com/pyc/java/board/Main.java
+++ src/main/java/com/pyc/java/board/Main.java
@@ -1,7 +1,16 @@
 package com.pyc.java.board;
 
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("== 자바 텍스트 게시판 시작 ==");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("명령) ");
+        String cmd = sc.nextLine();
+
+        System.out.printf("입력받은 명령어 : %s\n", cmd);
+        System.out.println("== 자바 텍스트 게시판 끝 ==");
+
     }
 }
