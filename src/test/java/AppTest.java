95d47213a4daf69d957843c54b7dc5d0e819d070
zerochani <youngchan0510@gmail.com>
2025-03-09 17:03:15 +0900

    쿼리스트링 추가할때마다 반복작업해야하는 문제 발생

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -2,22 +2,37 @@ import java.util.*;
 
 public class AppTest {
     public static void main(String[] args) {
-        String queryString = "id=1&subject=제목1&content=내용1&writerName=김철수&boardId=1";
+        String queryString1 = "id=1&subject=제목1&content=내용1&writerName=김철수&boardId=1";
 
-        String[] queryStringBits = queryString.split("&");
+        String[] queryString1Bits = queryString1.split("&");
+        String queryString2 = "id=2&subject=제목2&content=내용2&writerName=신유리&boardId=2";
+        String[] queryString2Bits = queryString2.split("&");
 
-        Map<String,String> params = new LinkedHashMap<>();
+        Map<String,String> params1 = new LinkedHashMap<>();
 
-        for(String bit : queryStringBits){
+        for(String bit : queryString1Bits){
             String[] bits = bit.split("=");
-            params.put(bits[0], bits[1]);
+            params1.put(bits[0], bits[1]);
         }
 
-        System.out.println(params);
+        Map<String,String> params2 = new LinkedHashMap<>();
+
+        for(String bit : queryString2Bits){
+            String[] bits = bit.split("=");
+            params2.put(bits[0], bits[1]);
+        }
+
+
+        System.out.println(params1);
+        System.out.println(params2);
 
         System.out.println("==반복문을 이용한 순회==");
-        params.forEach((key,value)->
+        params1.forEach((key,value)->
             System.out.printf("%s : %s\n", key,value));
+
+        System.out.println("==반복문을 이용한 순회==");
+        params2.forEach((key,value)->
+                System.out.printf("%s : %s\n", key,value));
 
     }
 }
