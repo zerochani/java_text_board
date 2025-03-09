f6c2cede00c651b11b83df4ca4d6e329cab1eba0
zerochani <youngchan0510@gmail.com>
2025-03-09 16:51:43 +0900

    list 두개이용하여 쿼리스트링을 다룰때 문제점

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -4,26 +4,33 @@ import java.util.List;
 
 public class AppTest {
     public static void main(String[] args) {
-        String queryString = "d=4&a=1&b=2&c=3&f=6";
+        String queryString = "id=1&subject=제목1&content=내용1&writerName=김철수&boardId=1";
 
         String[] queryStringBits = queryString.split("&");
 
         System.out.println(Arrays.toString(queryStringBits));
         List<String> paramsKey = new ArrayList<>();
-        List<Integer> paramsValue = new ArrayList<>();
+        List<String> paramsValue = new ArrayList<>();
 
         for(String bit : queryStringBits){
             String[] bits = bit.split("=");
             //System.out.println(Arrays.toString(bits));
-            paramsKey.add(bits[0]); //d a b c f
-            paramsValue.add(Integer.parseInt(bits[1])); //4 1 2 3 6
+            paramsKey.add(bits[0]);
+            paramsValue.add(bits[1]);
         }
-        for(int i=0; i<paramsKey.size(); i++){
-            String paramKey = paramsKey.get(i);
-            int paramValue = paramsValue.get(i);
+//        for(int i=0; i<paramsKey.size(); i++){
+//            String paramKey = paramsKey.get(i);
+//            String paramValue = paramsValue.get(i);
+//
+//            System.out.println("%s : %s\n", paramKey, paramValue);
+//        }
+        System.out.println(paramsKey);
+        System.out.println(paramsValue);
 
-            System.out.printf("%s : %d\n", paramKey, paramValue);
-        }
+        //subject 인덱스 구하기 및 키 subject의 값 구하기
+        int findIdx = paramsKey.indexOf("subject");
+
+        System.out.println(paramsValue.get(findIdx));
 
     }
 }
