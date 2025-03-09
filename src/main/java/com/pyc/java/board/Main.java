c495d01054e45431eeda2f87b95e141b37d1bdba
zerochani <youngchan0510@gmail.com>
2025-03-09 14:57:40 +0900

    게시물 상세보기 기능구현

--- src/main/java/com/pyc/java/board/Main.java
+++ src/main/java/com/pyc/java/board/Main.java
@@ -6,6 +6,8 @@ public class Main {
         System.out.println("== 자바 텍스트 게시판 시작 ==");
         Scanner sc = new Scanner(System.in);
         int lastId = 0; //게시물 번호
+        Article lastArticle = null;
+
         while(true){
             System.out.print("명령) ");
             String cmd = sc.nextLine();
@@ -22,10 +24,23 @@ public class Main {
                 int id = ++lastId;
 
                 Article article = new Article(id,subject,content);
+                lastArticle = article;
 
                 System.out.println("생성된 게시물 객체 : " + article);
 
                 System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
+            }else if(cmd.equals("/user/article/detail")){
+                Article article = lastArticle;
+
+                if(article == null){
+                    System.out.println("게시물이 존재하지 않습니다.");
+                    continue;
+                }
+
+                System.out.println("==게시물 상세보기 ==");
+                System.out.printf("번호: %d\n", article.id);
+                System.out.printf("제목: %s\n", article.subject);
+                System.out.printf("내용: %s\n", article.content);
             }else if(cmd.equals("exit")){
                 System.out.println("프로그램을 종료합니다.");
                 break;
