89ed2d9d73bb95aefb55114be4d0e4284ad83c09
zerochani <youngchan0510@gmail.com>
2025-03-09 17:16:31 +0900

    Util::getParams도입으로 코드유지보수성 향상

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -3,36 +3,26 @@ import java.util.*;
 public class AppTest {
     public static void main(String[] args) {
         String queryString1 = "id=1&subject=제목1&content=내용1&writerName=김철수&boardId=1";
-
-        String[] queryString1Bits = queryString1.split("&");
         String queryString2 = "id=2&subject=제목2&content=내용2&writerName=신유리&boardId=2";
-        String[] queryString2Bits = queryString2.split("&");
-
-        Map<String,String> params1 = new LinkedHashMap<>();
-
-        for(String bit : queryString1Bits){
-            String[] bits = bit.split("=");
-            params1.put(bits[0], bits[1]);
-        }
-
-        Map<String,String> params2 = new LinkedHashMap<>();
-
-        for(String bit : queryString2Bits){
-            String[] bits = bit.split("=");
-            params2.put(bits[0], bits[1]);
-        }
 
 
+        Map<String,String> params1 = Util.getParams(queryString1);
+        Map<String,String> params2 = Util.getParams(queryString2);
         System.out.println(params1);
         System.out.println(params2);
+    }
+}
 
-        System.out.println("==반복문을 이용한 순회==");
-        params1.forEach((key,value)->
-            System.out.printf("%s : %s\n", key,value));
+class Util{
+    static Map<String, String> getParams(String queryStr){
+        Map<String,String> params = new LinkedHashMap<>();
+        String[] queryStrBits = queryStr.split("&");
 
-        System.out.println("==반복문을 이용한 순회==");
-        params2.forEach((key,value)->
-                System.out.printf("%s : %s\n", key,value));
+        for(String Bit : queryStrBits){
+            String[] bit = Bit.split("=");
 
+            params.put(bit[0],bit[1]);
+        }
+        return params;
     }
 }
