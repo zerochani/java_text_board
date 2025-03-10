0ae61264f3ca3e9a53111275a6f8c58e5a205908
zerochani <youngchan0510@gmail.com>
2025-03-10 23:16:50 +0900

    id값에 대한 유효성 처리

--- src/main/java/com/pyc/java/board/Main.java
+++ src/main/java/com/pyc/java/board/Main.java
@@ -61,7 +61,19 @@ public class Main {
                 }
 
                 Map<String,String> params = rq.getParams();
-                int id = Integer.parseInt(params.get("id"));
+
+                if(!params.containsKey("id")){
+                    System.out.println("id 값을 입력해주세요.");
+                    continue;
+                }
+                int id = 0;
+                try{
+                    id = Integer.parseInt(params.get("id"));
+                }catch(NumberFormatException e){
+                    System.out.println("id를 정수형태로 입력해주세요.");
+
+                }
+
                 if(id>articles.size()){
                     System.out.printf("%d번 게시물은 존재하지 않습니다.\n",id);
                     continue;
