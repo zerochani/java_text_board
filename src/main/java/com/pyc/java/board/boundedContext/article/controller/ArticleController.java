4adf7ccde38d395e92a8d834adf98b182b0efefb
zerochani <youngchan0510@gmail.com>
2025-03-22 16:05:39 +0900

    게시물 수정,삭제시 권한체크

--- src/main/java/com/pyc/java/board/boundedContext/article/controller/ArticleController.java
+++ src/main/java/com/pyc/java/board/boundedContext/article/controller/ArticleController.java
@@ -29,9 +29,15 @@ public class ArticleController {
         }
 
         Article article = articleService.findById(id);
+        Member member = (Member) rq.getSessionAttr("loginedMember");
 
         if(article == null) {
             System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+            return;
+        }
+
+        if(!article.getWriterName().equals(member.getUsername())){
+            System.out.println("게시물에 접근권한이 없습니다.");
             return;
         }
 
@@ -50,9 +56,15 @@ public class ArticleController {
         }
 
         Article article = articleService.findById(id);
+        Member member = (Member) rq.getSessionAttr("loginedMember");
 
         if(article == null) {
             System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+            return;
+        }
+
+        if(!article.getWriterName().equals(member.getUsername())){
+            System.out.println("게시물에 접근 권한이 없습니다.");
             return;
         }
 
